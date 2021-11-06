07cc66eff15b959973af2cbdc2a7baf90de47ca9
CNFLKT7 <danilatit@yandex.ru>
2021-11-06 23:07:59 +0300

    Определил передаваемый формат

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace MyMessenger
 {
@@ -6,8 +7,15 @@ namespace MyMessenger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("CNFLKT", "Hello", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+
+            //{ "UserName":"CNFLKT","MessageText":"Hello","TimeStamp":"2021-11-06T20:07:03.192628Z"}
+            //CNFLKT < 06.11.2021 20:07:03 >: Hello
         }
     }
 }
